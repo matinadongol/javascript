4ed1f30253100201ee0dc1893f9013495929f1b2
matinadongol <dongolmt@gmail.com>
2021-06-03 11:48:08 +0545

    javascript

---
+++ javascript/js project/index.js
@@ -0,0 +1,13 @@
+// console.log("hello world");
+
+
+// for(var num = 0; num<=10; num++){
+//     debugger;
+//     console.log(num);
+// }
+
+
+// for(var n=1; n<=10; n++){
+//     num=8;
+//     console.log(num + '*' + n + "=" + num * n);
+// }
